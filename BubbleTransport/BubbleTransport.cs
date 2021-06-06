c5dd1cab8703c7975cbe8d64adae2c6b4cd29356
Matthew Collier <51029884+Squaresweets@users.noreply.github.com>
2021-06-06 19:19:47 +0100

    Add files via upload

--- BubbleTransport/BubbleTransport.cs
+++ BubbleTransport/BubbleTransport.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 using System.Runtime.InteropServices;
 using UnityEngine.Events;
 
@@ -65,16 +66,60 @@ public class BubbleTransport : Mirror.Transport
     public class InviteRecievedEvent : UnityEvent { }
     [SerializeField]
     private MatchFoundEvent inviteRecieved = new MatchFoundEvent();
-
-    [HideInInspector]
-    private Mirror.NetworkManager networkManager;
     
     bool available = true;
+
+    /* Structure of the transport
+    
+    Without an Invite:
+    - All players call the FindMatch() function
+    - Game Center searches for and finds a match
+    - When a match is found, which device should act as the host and the connID of all the clients is calculated (as seen below)
+    - Server:
+        - ServerStart() is called letting Unity know we are acting as a server
+        - For every player in the list of players ServerConnected() is called, this way the connID of each player is their position in the players array
+    - Client:
+        - The serverPlayer variable is set
+        - The client waits for 2, 3 or 4 seconds depending on its position in the players array
+            - This is to make players join in the right order and prevent clients from joining too early
+        - ClientStart() is called letting Unity know we are acting as a client
+    
+    With an Invite (https://www.youtube.com/watch?v=e-RCPvUYxr4 [Example has 3 players, four is also possible I just didn't have that many devices]):
+    - One player invites all other players
+    - On all other players OnInviteRecieved() is called, this shuts down the network if it is currently on one and calls FindMatch()
+    - FindMatch() instantiates the match from an invite
+    - Match continues as normal (see above)
+
+    Determining the Host:
+    - This is the section that probably took me the most time to figure out
+    - All players are added to a player array when they join
+    - All GKPlayers have a playerID
+    - The player array is sorted on this playerID, so the array is in the same order on all devices
+    - The first player in the array acts as the server, so a game with the same people will always have the same server
+
+    */
+
+
+
+    /// <summary>
+    /// <para/>This is one of the main functions you should worry about, you should call it when you want to open the matchmaking UI
+    /// <para/>Do not try to start a match by using ServerStart() or ClientConnect(), due to the nature of the randomness in matchmaking this will not work
+    /// <para/>Instead when gamecenter finds a match it will add all of the clients and call those functions when needed
+    /// </summary>
+    public void FindMatch()
+    {
+        if (Mirror.NetworkManager.singleton.maxConnections > 4)
+        {
+            Debug.LogError("Real time Game Center Matches support a max of 4 players");
+            return;
+        }
+        _FindMatch(MinPlayers, Mirror.NetworkManager.singleton.maxConnections);
+    }
 
     #region Other
     public override bool Available()
     {
-        return Application.platform == RuntimePlatform.IPhonePlayer && available;
+        return Application.platform == RuntimePlatform.IPhonePlayer && available && new Version(Device.systemVersion) >= new Version("13.0");
     }
 
     //~~~~~~~~~~ These two functions are all sorted out by game center, and these should not be called by anything other than the transport, if you want to start a game use FindMatch(); ~~~~~~~~~~
@@ -94,12 +139,13 @@ public class BubbleTransport : Mirror.Transport
     [AOT.MonoPInvokeCallback(typeof(OnInviteRecievedDelegate))]
     static void OnInviteRecieved()
     {
-        if(instance.networkManager.isNetworkActive)
+        //An invite has been recieved, we shutdown the network and then call FindMatch
+        if(Mirror.NetworkManager.singleton.isNetworkActive)
         {
             if (Mirror.NetworkServer.active)
-                instance.networkManager.StopHost();
+                Mirror.NetworkManager.singleton.StopHost();
             else
-                instance.networkManager.StopClient();
+                Mirror.NetworkManager.singleton.StopClient();
         }
         instance.inviteRecieved.Invoke();
 
@@ -111,7 +157,7 @@ public class BubbleTransport : Mirror.Transport
     #region Client
     public override bool ClientConnected()
     {
-        throw new NotImplementedException();
+        return Mirror.NetworkManager.singleton.isNetworkActive && !Mirror.NetworkServer.active;
     }
 
     public override void ClientDisconnect()
@@ -135,7 +181,6 @@ public class BubbleTransport : Mirror.Transport
     [AOT.MonoPInvokeCallback(typeof(OnClientDisconnectedDelegate))]
     static void ClientDisconnectedCallback()
     {
-        print("Client disconnected callback");
         instance.OnClientDisconnected.Invoke();
     }
 
@@ -143,6 +188,19 @@ public class BubbleTransport : Mirror.Transport
     [AOT.MonoPInvokeCallback(typeof(OnClientDidDataRecievedDelegate))]
     static void OnClientDidDataRecieved(IntPtr data, int offset, int count)
     {
+        /*
+        We get a pointer back from the plugin containing the location of the array of bytes
+        Data recieved is formatted like this:
+
+        -----------------------------------------------------
+        |        |                                          |
+        | offset |     Byte Array containing all data...    |
+        | 1 Byte |     Rest of the array                    |
+        |        |                                          |
+        -----------------------------------------------------
+
+        Objective C code splits this up into offset and count and sends it here
+        */
         byte[] _data = new byte[count];
         Marshal.Copy(data, _data, 0, count);
         instance.OnClientDataReceived.Invoke(new ArraySegment<byte>(_data, offset, count), 0);
@@ -153,7 +211,7 @@ public class BubbleTransport : Mirror.Transport
     static void OnClientStartCallback()
     {
         instance.matchFound.Invoke();
-        instance.networkManager.StartClient();
+        Mirror.NetworkManager.singleton.StartClient();
         instance.OnClientConnected.Invoke();
     }
     #endregion
@@ -161,12 +219,12 @@ public class BubbleTransport : Mirror.Transport
     #region Server
     public override bool ServerActive()
     {
-        throw new NotImplementedException();
+        return Mirror.NetworkManager.singleton.isNetworkActive && Mirror.NetworkServer.active;
     }
 
     public override bool ServerDisconnect(int connectionId)
     {
-        //Game center matchmaking does not support kicking a client
+        //Game center matchmaking does not support kicking a client, you may want to create your own functionallity for telling a client to disconnect
         return false;
     }
 
@@ -194,6 +252,19 @@ public class BubbleTransport : Mirror.Transport
     [AOT.MonoPInvokeCallback(typeof(OnServerDidDataRecievedDelegate))]
     static void OnServerDidDataRecieved(int connId, IntPtr data, int offset, int count)
     {
+        /*
+        We get a pointer back from the plugin containing the location of the array of bytes
+        Data recieved is formatted like this:
+
+        -----------------------------------------------------
+        |        |                                          |
+        | offset |     Byte Array containing all data...    |
+        | 1 Byte |     Rest of the array                    |
+        |        |                                          |
+        -----------------------------------------------------
+
+        Objective C code splits this up into offset and count and sends it here
+        */
         byte[] _data = new byte[count];
         Marshal.Copy(data, _data, 0, count);
         instance.OnServerDataReceived.Invoke(connId, new ArraySegment<byte>(_data, offset, count), 0);
@@ -214,14 +285,14 @@ public class BubbleTransport : Mirror.Transport
     static void OnServerStartCallback()
     {
         instance.matchFound.Invoke();
-        instance.networkManager.StartHost();
+        Mirror.NetworkManager.singleton.StartHost();
     }
 
     delegate void ServerStopDelegate();
     [AOT.MonoPInvokeCallback(typeof(ServerStopDelegate))]
     static void ServerStopCallback()
     {
-        instance.networkManager.StopHost();
+        Mirror.NetworkManager.singleton.StopHost();
     }
 
     public override void ServerStop()
@@ -231,30 +302,8 @@ public class BubbleTransport : Mirror.Transport
 
     #endregion
 
-    /// <summary>
-    /// <para/>This is one of the main functions you should worry about, you should call it when you want to open the matchmaking UI
-    /// <para/>Do not try to start a match by using ServerStart() or ClientConnect(), due to the nature of the randomness in matchmaking this will not work
-    /// <para/>Instead when gamecenter finds a match it will add all of the clients and call those functions when needed
-    /// </summary>
-    public void FindMatch()
-    {
-        if(networkManager.maxConnections > 4)
-        {
-            Debug.LogError("Real time Game Center Matches support a max of 4 players");
-            return;
-        }
-        _FindMatch(MinPlayers, networkManager.maxConnections);
-    }
-
     private void Awake()
     {
-        try{
-            networkManager = GetComponent<Mirror.NetworkManager>();
-        }
-        catch{
-            Debug.LogError("Nework Manager could not be found");
-        }
-
         if (instance == null)
             instance = this;
 
@@ -273,6 +322,7 @@ public class BubbleTransport : Mirror.Transport
             _InitGameCenter();
         }
         catch{
+            //If you get an error while registering them than the transport is not available
             available = false;
         }
     }
